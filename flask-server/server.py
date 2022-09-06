01068c5699ac1df724aa94ca41c61830418c4b73
Mohamed Hassan <mdhn6832@gmail.com>
2022-09-06 19:24:43 +0200

    add the basis for add_project function
    fix other minor bugs and add comments to document it better

--- flask-server/server.py
+++ flask-server/server.py
@@ -6,15 +6,18 @@ it has a list of api calls that allows
 the react app to interact with the database
 """
 
-import json
 import sqlite3
 from flask import(
             Flask,
             )
 import helpers.database as DB
+from helpers.project import Project
 
 app = Flask(__name__)
 # opens a connection with the database
+# Check same thread = False allows to operate on the db from diffrent threads which can be bad for concurrency in a scale
+# but that is okay as its a local project for now.
+# detect_types is used to allow to store the data in the database as a form of python object as datetime obj for example
 db = sqlite3.connect('./database/db.sqlite3', check_same_thread=False)
 # opens a cursor to be able to control the database and query it
 cur = db.cursor()
@@ -22,26 +25,34 @@ cur = db.cursor()
 cur.executescript( open("./database/schema.sql", encoding="UTF-8").read() )
 
 @app.route("/projects")
-def retrieve_projects() -> json:
+def retrieve_projects() -> dict:
     """ retrives the data of the projects created by the user from the database.
 
     Returns:
         dict: The projects in the form of json Object.
     """
-    # request.data("user_id")
+    # TODO REPLACE 1 with the user_id coming from the request.
     return DB.get_all_projects(cur, 1)
 
 
 @app.route('/add', methods=["POST"])
-def add_project() -> bool:
+def add_project() -> dict:
     """ Adds a new project to the database
 
     Returns:
-        bool: indicating whether or not the operation was successful.
+        dict: Formatted as json, first value is a bool wheteher it worked or not.
+                                 the second value is a message to be displayed. 
     """
-    # TODO Build a function to add a project
-    # it should return a bool to determine whether or not it was successful
-    return True
+    #pylint: disable=broad-except
+
+    # TODO get data from request.
+    data = {}
+    proj = Project(**data)
+    try:
+        DB.add_project(cur, proj)
+    except Exception as err_msg:
+        return {"result": [0, err_msg]}
+    return {"result": [1, "The project has been added succesfully"]}
 
 
 @app.route('/img', methods=["POST"])
