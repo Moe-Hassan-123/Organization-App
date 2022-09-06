8695136e5d9787798fe2dbaafa1168d10cc1e2e7
Mohamed Hassan <mdhn6832@gmail.com>
2022-09-06 08:25:52 +0200

    add structure to the tests

---
+++ testing/scripts/test_deletion.py
@@ -0,0 +1,10 @@
+"""Tests the Adding Project Function
+"""
+import sys
+sys.path.append('/home/mohamed/code/Organization Website/flask-server/helpers')
+
+# from database import delete_project
+
+...
+
+ 
---
+++ testing/scripts/test_get_all_projects.py
@@ -0,0 +1,12 @@
+"""Tests the Adding Project Function
+"""
+import sys
+sys.path.append('/home/mohamed/code/Organization Website/flask-server/helpers')
+
+from database import get_all_projects
+
+# place holder untill actual tests are written
+# its here so the git commit hook can execute.
+def test_plus():
+    assert 1+1 == 2
+ 
