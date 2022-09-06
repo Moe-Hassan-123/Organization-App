c28fb7b24e5ed965834d7455ac45dfc674dcc1d4
Mohamed Hassan <mdhn6832@gmail.com>
2022-09-06 19:38:20 +0200

    implement a delete endpoint to delete a project from the db

--- flask-server/server.py
+++ flask-server/server.py
@@ -12,12 +12,13 @@ from flask import(
             )
 import helpers.database as DB
 from helpers.project import Project
+import project
 
 app = Flask(__name__)
+
 # opens a connection with the database
-# Check same thread = False allows to operate on the db from diffrent threads which can be bad for concurrency in a scale
-# but that is okay as its a local project for now.
-# detect_types is used to allow to store the data in the database as a form of python object as datetime obj for example
+# Check same thread = False allows to operate on the db from diffrent threads -
+# which can be bad for concurrency in a scale but that is okay as its a local project for now.
 db = sqlite3.connect('./database/db.sqlite3', check_same_thread=False)
 # opens a cursor to be able to control the database and query it
 cur = db.cursor()
@@ -62,8 +63,30 @@ def get_user_data():
     Returns:
         dict: formatted as json with the name and image of the user.
     """
-    # user_id = request.data("user_id")
+    # TODO since i have a no authentication sign-in in my mind
+    # the user id will be a value attached to a button that when clicked
+    # will call this function and pass the user_id in a post request.
+    # from 1 to 6 perhaps for 6 users.
+    # this can be the basis for a sign-in system but its not needed as its a local project.
     return DB.fetch_user(db, 0)
+
+@app.route('/delete', methods=["POST"])
+def method_name():
+    """
+    Deletes a project from the database.
+    Returns:
+        2-list:
+        [0]:  bool: indicates whether the insertion was successful or not.
+        [1]:  str: a message to be displayed
+    """
+    ## TODO GET THESE VALUES FROM THE REQUEST
+    user_id = project_id = 0
+    try:
+        DB.delete_project(cur, user_id, project_id)
+    except Exception as err_msg:
+        return {"result": [0, err_msg]}
+    return {"result": [1, "The Project was deleted succesfully"]}
+
 
 if __name__ == "__main__":
     app.run(debug=True, port=12221)
